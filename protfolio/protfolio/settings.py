237e188b02217e734a9ffcbae0b5ef46d01b358f
Abiral Bhattarai <70887103+AbiralBhattarai@users.noreply.github.com>
2026-02-10 13:43:48 +0545

    Change DEBUG setting to False
    Set DEBUG to False for production environment.

--- protfolio/protfolio/settings.py
+++ protfolio/protfolio/settings.py
@@ -28,7 +28,7 @@ TEMPLATE_DIR = BASE_DIR/'templates'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 
 
